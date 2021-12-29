e4bc3e79c050886692e9ea32b050737112ec2e3d
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-12-29 17:20:04 -0300

    Criação da classe que contem os endpoints

---
+++ BibliotecaAPI/Endpoints/AutoresEndpoints.cs
@@ -0,0 +1,122 @@
+﻿
+using BibliotecaAPI.Data;
+using Dapper.Contrib.Extensions;
+using static BibliotecaAPI.Data.BancoContext;
+
+namespace BibliotecaAPI.Endpoints
+{
+    public static class AutoresEndpoints
+    {
+        public static void MapAutoresEndpoints(this WebApplication app)
+        {
+            app.MapGet("/autores", async (GetConnection connectionGetter) =>
+            {
+                using var con = await connectionGetter();
+                List<Autores> autores = new List<Autores>();
+                try
+                {
+                    autores = con.GetAll<Autores>().ToList();
+                    
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                    return Results.StatusCode(500);
+                }
+                
+                if(autores is null)
+                {
+                    return Results.NotFound();
+
+                }
+                return Results.Ok(autores);
+            });
+
+            app.MapGet("/autores/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                Autores autor;
+                try
+                {
+                    autor = con.Get<Autores>(id);
+                    if(autor is null)
+                    {
+                        return Results.NotFound();
+                    }
+                }
+                catch(Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.ToString());
+                    return Results.StatusCode(500);
+                }
+                return Results.Ok(autor);
+            });
+
+            app.MapPost("/autores", async(GetConnection connectionGetter,Autores autor) =>
+            {
+                using var con = await connectionGetter();
+                if(autor is null)
+                {
+                    return Results.BadRequest();
+                }
+                try
+                {
+                    var id = con.Insert<Autores>(autor);
+                    return Results.Created($"/autores/{id}",autor);
+                }
+                catch(Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.ToString());
+                    return Results.StatusCode(500);
+                }
+            });
+
+            app.MapPut("/autores", async (GetConnection connectionGetter, Autores autor) =>
+            {
+                using var con = await connectionGetter();
+                if(autor is null)
+                {
+                    return Results.BadRequest();
+                }
+
+                try
+                {
+                    con.Update<Autores>(autor);
+                    
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.ToString());
+                    return Results.StatusCode(500);
+                }
+                return Results.Ok();
+            });
+
+            app.MapDelete("/autores/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                var deleted = con.Get<Autores>(id);
+
+                if(deleted is null)
+                {
+                    return Results.NotFound();
+                }
+
+
+                try
+                {
+                    con.Delete<Autores>(deleted);
+                }
+                catch(Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex);
+                    return Results.StatusCode(500);
+                }
+
+                
+                return Results.Ok();
+                
+            });
+        }
+    }
+}

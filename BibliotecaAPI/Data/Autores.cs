23c341d7499c891c445d97fc425cfc8ecfdc8e21
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-12-29 16:51:33 -0300

    O context do banco agora é único, independentemente da tabela

---
+++ BibliotecaAPI/Data/Autores.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace BibliotecaAPI.Data
+{   [Table("Autores")]
+    public record Autores
+    (
+        System.Int32 Id,
+        System.String Nome
+    );
+        
+
+    
+}
--- BibliotecaAPI/Data/BancoContext.cs
+++ BibliotecaAPI/Data/BancoContext.cs
@@ -3,7 +3,7 @@ using System.Data;
 
 namespace BibliotecaAPI.Data
 {
-    public class LivrosContext
+    public class BancoContext
     {
         //Aponta para a função que faz a conexão com o banco
         public delegate Task<IDbConnection> GetConnection();

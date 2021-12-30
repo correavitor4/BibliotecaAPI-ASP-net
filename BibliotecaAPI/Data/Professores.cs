89c4555e5c895501a4c708be6c30e0da33f0a504
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-12-30 13:21:43 -0300

    Criar um "record" para a entidade "Professores"

---
+++ BibliotecaAPI/Data/Professores.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BibliotecaAPI.Data
+{
+    [Table("Professores")]
+    public record Professores(
+        System.String Nome,
+        System.String Endereco,
+        System.String Telefone,
+        System.String Telefone_celular,
+        System.String CPF,
+        System.String RG,
+        System.String Email,
+        System.Int32 Id
+        );
+}

bd2b223c3de20dec96cb8ef257bb0891028e6ff7
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-12-30 17:06:46 -0300

    Criação do "record" da entidade "Alunos"

---
+++ BibliotecaAPI/Data/Alunos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BibliotecaAPI.Data
+{
+    [Table("Alunos")]
+    public record Alunos(
+        System.Int32 Id,
+        System.String Telefone,
+        System.String CPF,
+        System.String RG,
+        System.String Endereco,
+        System.String Telefone_celular,
+        System.String Email,
+        System.String Nome
+    );
+}

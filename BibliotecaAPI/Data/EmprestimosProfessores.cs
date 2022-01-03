a1358c9ac0615e2a9dbb4ce160a6db4c2edc10e2
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2022-01-03 19:08:06 -0300

    Criação do "record" correspondente à tabela de empréstimos para professores, do banco

---
+++ BibliotecaAPI/Data/EmprestimosProfessores.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BibliotecaAPI.Data
+{
+    [Table("EmprestimosDeProfessores")]
+    public record EmprestimosProfessores(
+        System.Int32 Id,
+        System.Int32 Fk_professor,
+        System.DateTime Data_do_emprestimo,
+        System.DateTime Data_prevista_devolucao,
+        System.Int32 Exemplar_emprestado 
+    );
+}
